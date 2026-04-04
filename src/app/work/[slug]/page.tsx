8e7152ab646cbe013898923307a286100a10a4e5
mikeknows <plymire.2@wright.edu>
2026-04-04 15:15:38 -0500

    Fix missing CommentsSection import on work detail page

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Meta, Schema, AvatarGroup, Button, Column, Flex, Heading, Media, Text }
 import { baseURL, about, person, work } from "@/resources";
 import { formatDate } from "@/utils/formatDate";
 import { ScrollToHash, CustomMDX } from "@/components";
+import { CommentsSection } from "@/components/comments/CommentsSection";
 import { Metadata } from "next";
 
 export async function generateStaticParams(): Promise<{ slug: string }[]> {
